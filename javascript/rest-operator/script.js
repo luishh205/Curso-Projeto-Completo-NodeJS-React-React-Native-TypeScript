bff70835e70c8491a752fe0b08432fa3eb1fa0b7
Luis Henrique Ramos Soares <89323852+luishh205@users.noreply.github.com>
2023-08-14 10:30:05 -0300

    Add files via upload

---
+++ javascript/rest-operator/script.js
@@ -0,0 +1,21 @@
+
+// REST Operator.
+
+// function convidados(...nomes){
+//   console.log("SEJA BEM VINDO TODOS CONVIDADOS")
+//   console.log(nomes);
+// }
+
+// convidados("Matheus", "Lucas", "Maria", "Ana caroline")
+
+
+function sorteador(...numeros){
+  console.log(numeros);
+
+  const numeroGerado = Math.floor(Math.random() * numeros.length);
+
+  console.log("Numero gerado foi: " + numeros[numeroGerado]);
+
+}
+
+sorteador(1, 4, 5, 15, 25, 90, 55, 34)
---
+++ javascript/spread-operator/script.js
@@ -0,0 +1,33 @@
+
+// Spread Operator.
+// let primeiros = [1, 2, 3];
+
+// let numeros = [...primeiros, 4, 5, 10];
+// console.log(numeros);
+
+// let pessoa = {
+//  nome: "Matheus",
+//  idade: 45,
+//  cargo: "RH"
+// }
+
+// let novaPessoa = {
+//   ...pessoa,
+//   status: "ATIVO",
+//   cidade: "Campo Grande / MS",
+//   telefone: "67959595959"
+// };
+// console.log(novaPessoa);
+
+function novoUsuario(info){
+  let dados = {
+    ...info,
+    status: "ATIVO",
+    inicio: "20/03/2023",
+    codigo: "123123"
+  };
+
+  console.log(dados);
+}
+
+novoUsuario({ nome: "Jose", sobrenome: "Silva", cargo: "DEV" })
